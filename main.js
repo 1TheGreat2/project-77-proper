46327a2c153d8315e8e36d1739f782ec4a35e43b
1TheGreat2 <83286527+1TheGreat2@users.noreply.github.com>
2022-10-25 17:49:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+var first_name = "Ryan";
+var last_name = "Saket";
+var string = "My first console project";
+var a = 10;
+var b = 5;
+var c = 2;
+var d = 3;
+var e = 25;
+var f = 25;
+var g = 1;
+
+var add1 = a+b;
+var add2 = c+d;
+var add3 = e+f;
+var add4 = g+g;
+
+console.log(first_name.concat(" ",last_name));
+console.log(string);
+console.log(add1);
+console.log(add2);
+console.log(add3);
+console.log(add4);
+
